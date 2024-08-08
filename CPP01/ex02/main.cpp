87591eddb2a8ba5ced100cce202a3aff7e0d0235
aboubakrbkd <bkd.bakr3@gmail.com>
2024-08-08 15:31:18 +0100

    finish ex02

---
+++ CPP01/ex02/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    std::string str = "HI THIS IS BRAIN";
+    std::string *stringPTR = &str;
+    std::string &stringREF = str;
+
+    std::cout <<"The memory address of the string variable: " << &str << std::endl;
+    std::cout <<"The memory adress held by stringPTR: " << stringPTR << std::endl;
+    std::cout <<"The memory adress held by stringREF: " << &stringREF << std::endl;
+
+    std::cout << "-----------------------------------------" << std::endl;
+
+    std::cout <<"The value of the string variable: " << str << std::endl;
+    std::cout <<"The value pointer by stringPTR: " << *stringPTR << std::endl;
+    std::cout <<"The value pointer by stringREF: " << stringREF << std::endl;
+}
