d2f7485379a0d1cc439a8d3a3af21c7c6546ec1b
aboubakr <bkd.bakr3@gmail.com>
2024-12-05 14:17:46 +0100

    finish ex00

--- CPP08/ex00/easyfind.hpp
+++ CPP08/ex00/easyfind.hpp
@@ -1,4 +1,18 @@
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::size_t function(T& arr, int occ)
+{
+    for (std::size_t i = 0; i < arr.size(); i++)
+    {
+        if (arr[i] == occ)
+            return (i);
+    }
+    throw std::runtime_error("Element not found");
+}
+
 #endif
--- CPP08/ex00/main.cpp
+++ CPP08/ex00/main.cpp
@@ -1,1 +1,23 @@
 #include "easyfind.hpp"
+
+int main()
+{
+    std::vector<int> v;
+
+    v.push_back(15);
+    v.push_back(20);
+    v.push_back(355);
+    v.push_back(432);
+    v.push_back(5234);
+
+    try {
+        std::size_t i = function(v, 15);
+        std::cout << "Element found at index: " << i << std::endl;
+        std::size_t f = function(v, 4);
+        std::cout << "Element found at index: " << f << std::endl;
+    }
+    catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
+
+}
