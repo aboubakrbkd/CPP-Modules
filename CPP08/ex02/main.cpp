92dab13ddf2b723de57ab63a7a054e5f2178cbcb
aboubakrbkd <bkd.bakr3@gmail.com>
2024-12-06 13:25:51 +0100

    added more tests to main

--- CPP08/ex02/main.cpp
+++ CPP08/ex02/main.cpp
@@ -12,6 +12,7 @@ int main()
 	mstack.push(5);
 	mstack.push(737);
 	mstack.push(0);
+	std::cout << "----Iterator----" << std::endl;
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
 	++it;
@@ -20,6 +21,30 @@ int main()
 	{
 		std::cout << *it << std::endl;
 		++it;
+	}
+	std::cout << "-----Const_Iterator----" << std::endl;
+	MutantStack<int>::const_iterator cit = mstack.begin();
+	MutantStack<int>::const_iterator cite = mstack.end();
+	while (cit != cite)
+	{
+		std::cout << *cit << std::endl;
+		++cit;
+	}
+	std::cout << "-----Reverse_Iterator-----" << std::endl;
+	MutantStack<int>::reverse_iterator rit = mstack.rbegin();
+	MutantStack<int>::reverse_iterator rite = mstack.rend();
+	while (rit != rite)
+	{
+		std::cout << *rit << std::endl;
+		++rit;
+	}
+	std::cout << "-----Reverse_const_iterator-----" << std::endl;
+	MutantStack<int>::const_reverse_iterator rcit = mstack.rbegin();
+	MutantStack<int>::const_reverse_iterator rcite = mstack.rend();
+	while (rcit != rcite)
+	{
+		std::cout << *rcit << std::endl;
+		++rcit;
 	}
 	std::stack<int> s(mstack);
 	return 0;
