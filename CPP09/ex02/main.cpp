d5526c658140f49a089b09468b48cef20890b8b8
Aboubakr Boukdidi <aboukdid@e2r2p1.1337.ma>
2024-12-13 08:11:43 +0100

    fi multiple printing

--- CPP09/ex02/main.cpp
+++ CPP09/ex02/main.cpp
@@ -2,17 +2,23 @@
 
 int main(int argc, char **argv)
 {
-	if (argc == 1)
-	{
-		std::cerr << "The Program accept ./PmergeMe ..." << std::endl;
-		return (1);
-	}
-	else
-	{
-		PmergeMe me;
-		for (int i = 1; i < argc; i++)
-			me.Ford_johnson(argv[i]);
-	}
+    if (argc == 1)
+    {
+        std::cerr << "The Program accepts ./PmergeMe ..." << std::endl;
+        return (1);
+    }
+    else
+    {
+        PmergeMe me;
+        std::string combinedArgs;
+        for (int i = 1; i < argc; i++)
+        {
+            if (i > 1)
+                combinedArgs += " ";
+            combinedArgs += argv[i];
+        }
+        me.Ford_johnson(combinedArgs);
+    }
 
+    return 0;
 }
-
