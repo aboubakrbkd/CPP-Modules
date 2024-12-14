4f28a3cd4601bd2db33e7ca51e06f7e0250b47e9
aboubakr <bkd.bakr3@gmail.com>
2024-12-14 19:38:19 +0100

    trying to add jacon

--- CPP09/ex02/PmergeMe.cpp
+++ CPP09/ex02/PmergeMe.cpp
@@ -6,6 +6,20 @@ PmergeMe::PmergeMe(const PmergeMe& obj) {(void)obj;};
 PmergeMe& PmergeMe::operator=(const PmergeMe& obj){(void)obj; return *this;};
 
 
+std::vector<int> GenerateJacobsthalSequence(std::size_t n)
+{
+    std::vector<int> jacob_seq;
+    jacob_seq.push_back(0);
+    jacob_seq.push_back(1);
+
+    for (std::size_t i = 2; i < n; i++)
+    {
+        std::size_t next = jacob_seq[i - 1] + 2 * jacob_seq[i - 2];
+        jacob_seq.push_back(next);
+    }
+    return (jacob_seq);
+}
+
 bool isValidNumber(const std::string& result)
 {
 	if (result.empty())
